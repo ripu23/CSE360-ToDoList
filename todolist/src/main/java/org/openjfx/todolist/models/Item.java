5f1cd5f391e552b68657dc5af6ca12384c1a4ac2
tncolli3 <47932219+tncolli3@users.noreply.github.com>
2019-04-20 12:20:28 -0500

    Update Item.java

--- todolist/src/main/java/org/openjfx/todolist/models/Item.java
+++ todolist/src/main/java/org/openjfx/todolist/models/Item.java
@@ -1,91 +1,68 @@
-package org.openjfx.todolist.models;
-
-import javafx.scene.control.Button;
+package org.openjfx.todolist;
 
 public class Item {
-
-	private int id;
+	
 	private String description;
 	private String dueDate;
 	private int priority;
 	private String status;
-	private String dateStarted;
-	private String dateFinished;
-	private Button delete;
-	private Button edit;
-
+	private String statusDate;  //date started or finished 
+	
+	//default constructor
 	public Item() {
-
+		description = "";
+		dueDate ="";
+		priority = 0;
+		status = "";
+		statusDate = "";
 	}
-
-	public Item(int id, String description, String dueDate, int priority, String status, String dateStarted,
-			String dateFinished, Button delete, Button edit) {
-		super();
-		this.id = id;
-		this.description = description;
-		this.dueDate = dueDate;
-		this.priority = priority;
-		this.status = status;
-		this.dateStarted = dateStarted;
-		this.dateFinished = dateFinished;
-		this.delete = delete;
-		this.edit = edit;
+	
+	public Item(String description,String dueDate, int priority, String status, String statusDate) {
+		this.description=description;
+		this.dueDate=dueDate;
+		this.priority=priority;
+		this.status=status;
+		this.statusDate=statusDate;
 	}
-
-	public String getDateStarted() {
-		return dateStarted;
-	}
-
-	public void setDateStarted(String dateStarted) {
-		this.dateStarted = dateStarted;
-	}
-
-	public String getDateFinished() {
-		return dateFinished;
-	}
-
-	public void setDateFinished(String dateFinished) {
-		this.dateFinished = dateFinished;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
+	
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
+	
 	public void setDueDate(String dueDate) {
 		this.dueDate = dueDate;
 	}
-
+	
 	public void setPriority(int priority) {
 		this.priority = priority;
 	}
-
+	
 	public void setStatus(String status) {
 		this.status = status;
 	}
-
+	
+	public void setStatusDate(String statusDate) {
+		this.statusDate = statusDate;
+	}
+	
 	public String getDescription() {
 		return this.description;
 	}
-
+	
 	public String getDueDate() {
 		return this.dueDate;
 	}
-
+	
 	public int getPriority() {
 		return this.priority;
 	}
-
+	
 	public String getStatus() {
 		return this.status;
+	}
+	
+	public String getStatusDate() {
+		return this.statusDate;
 	}
 
 }
